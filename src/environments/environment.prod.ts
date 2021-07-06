f00b652f689c18c9bf5e7700094022954dc2c846
bnour <nborchani@welyne.com>
2021-07-06 16:02:52 +0200

    add firebase config in environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBo1N0cAFQM7yM0BmlBg3sYYrBQJtL_b0s",
+    authDomain: "t4u-nour.firebaseapp.com",
+    projectId: "t4u-nour",
+    storageBucket: "t4u-nour.appspot.com",
+    messagingSenderId: "1001707598582",
+    appId: "1:1001707598582:web:29da158300e5c04c0772d7",
+    measurementId: "G-X3Y22G6CQ6"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBo1N0cAFQM7yM0BmlBg3sYYrBQJtL_b0s",
+    authDomain: "t4u-nour.firebaseapp.com",
+    projectId: "t4u-nour",
+    storageBucket: "t4u-nour.appspot.com",
+    messagingSenderId: "1001707598582",
+    appId: "1:1001707598582:web:29da158300e5c04c0772d7",
+    measurementId: "G-X3Y22G6CQ6"
+  }
 };
 
 /*
